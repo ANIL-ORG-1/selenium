273b11d1df136952be1fee068e1123f34fb0e090
Hari Kammana <hari.kammana@gmail.com>
2018-11-01 11:49:48 +0530

    Demo

---
+++ src/test/java/selenium/selenium/AppTest.java
@@ -0,0 +1,50 @@
+package selenium.selenium;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	public void testApp() {
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream("../config.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+			System.out.println(prop.getProperty("database"));
+			System.out.println(prop.getProperty("dbuser"));
+			System.out.println(prop.getProperty("dbpassword"));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+}
